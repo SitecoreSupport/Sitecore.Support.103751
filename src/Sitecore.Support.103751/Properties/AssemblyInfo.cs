266dc1fa3a217af1de1f07a7788d35c68cb01272
Dmitry Savushkin <dmsa@sitecore.net>
2017-03-20 14:09:35 +0200

    Commit

---
+++ src/Sitecore.Support.103751/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.103751")]
+[assembly: AssemblyProduct("Sitecore.Support.103751")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.103751/Shell/Applications/WebEdit/WebEditRibbonForm.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Events;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.WebEdit;
+using Sitecore.Web.UI.Sheer;
+using System;
+
+namespace Sitecore.Support.Shell.Applications.WebEdit
+{
+    public class WebEditRibbonForm: Sitecore.Shell.Applications.WebEdit.WebEditRibbonForm
+    {
+        private ID parentID;
+
+        protected override void DeletedNotification(object sender, ItemDeletedEventArgs args)
+        {
+            base.DeletedNotification(sender, args);
+            this.parentID = args.ParentID;
+        }
+
+        protected new void Redirect(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.IsNotNullOrEmpty(args.Parameters["url"], "url");
+            ItemPath paths = Database.GetDatabase("master").GetItem(this.parentID).Paths;
+            SheerResponse.Eval($"window.parent.location.href='{paths.Path}?sc_mode=edit'");
+        }
+    }
+}
